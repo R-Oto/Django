2ccbaeb802139bb82bdfaf445877a38dbf65c08c
R.Oto <r.obusinesscommercial@gmail.com>
2024-09-30 19:31:18 -0700

    django part-4

--- learn/project/posts/models.py
+++ learn/project/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(upload_to='posts/')
+
+    def __str__(self):
+        return self.title
