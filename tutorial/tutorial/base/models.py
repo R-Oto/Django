40c0051e44d12d807aeaed923f830a8faa3cbd4a
R.Oto <r.obusinesscommercial@gmail.com>
2024-09-30 20:53:06 -0700

    django tutorial

--- tutorial/tutorial/base/models.py
+++ tutorial/tutorial/base/models.py
@@ -6,3 +6,9 @@ class Tour(models.Model):
     destination_country = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.origin_country} to {self.destination_country} for {self.number_of_nights} nights - ${self.price}"
+
+    class Meta:
+        verbose_name_plural = "Tours"
--- tutorial/tutorial/base/views.py
+++ tutorial/tutorial/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Tour
 # Create your views here.
 def index(request):
-    return render(request, '')
+    tours = Tour.objects.all()
+    return render(request, 'tours/index.html', {'tours':tours})
