4a911f4bbe914a938d87c57e3b1f642b6512e92e
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-01 00:41:51 -0700

    django tutorial

--- tutorial/tutorial/base/urls.py
+++ tutorial/tutorial/base/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('contact/success/', views.contact_success_view, name='contact-success'),
 ]
