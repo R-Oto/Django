82befee42da8d3617bcc8e2f72ab1f6f68c006e9
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-03 19:57:04 +0400

    weather app completed

--- basic-weather/weather/base/views.py
+++ basic-weather/weather/base/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 import json 
 import urllib.request
-# Create your views here.
+
 def index(request):
+    data = {}
+    city = ''
+    
     if request.method == "POST":
-        city = request.POST['city']
-        req = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=1b88f27e8c9f835364dc37e8486c83d8')
-    else:
-        city = ''
-    return render(request, 'index.html', {'city':city})
+        city = request.POST.get('city', '')
+        try:
+            api_key = '1b88f27e8c9f835364dc37e8486c83d8'  # Make sure to secure your API key
+            res = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric').read()
+            json_data = json.loads(res)
+            
+            data = {
+                "country_code": str(json_data['sys']['country']),
+                "coordinate": f"{json_data['coord']['lon']} {json_data['coord']['lat']}",
+                "temp": f"{json_data['main']['temp']} °C",  # Display temperature in Celsius
+                "pressure": str(json_data['main']['pressure']),
+                "humidity": str(json_data['main']['humidity'])
+            }
+        except Exception as e:
+            data = {"error": "Could not retrieve data for that city. Please check the city name."}
+
+    return render(request, 'index.html', {'data': data, 'city': city})
