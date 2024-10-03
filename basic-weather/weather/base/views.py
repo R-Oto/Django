9290a89109d2762fa0f2dc5eb10ab5c8f16f9b97
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-03 19:43:11 +0400

    basic weather

--- basic-weather/weather/base/views.py
+++ basic-weather/weather/base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+import json 
+import urllib.request
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        city = request.POST['city']
+        req = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=1b88f27e8c9f835364dc37e8486c83d8')
+    else:
+        city = ''
+    return render(request, 'index.html', {'city':city})
