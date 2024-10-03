3c74cefa65a87a2cb37c578a37f51ca8bd111b1d
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-03 20:06:08 +0400

    adding templates

--- chat-app/chat/base/views.py
+++ chat-app/chat/base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
+
+def room(request):
+    return render(request, 'room.html')
