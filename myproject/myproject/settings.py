bbe8882bba11cfd3e7639681d17a3f5821d25334
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-06 20:04:58 +0400

    models

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-(2_9+kts)07ikaqq0fx_d9i*%qt01v4h8zrvk7*&@9iqb@i!6-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
